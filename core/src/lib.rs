c74f4693e87eafa0ef1032fcff9791f1f86db26d
Bas v.d. Wiel <bas@area536.com>
2025-01-03 11:37:40 +0100

    Wired the old manifest back in for now.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,11 +1,16 @@
 use std::path::Path;
 
+use manifest::Manifest;
+
 #[derive(Debug)]
 pub struct DosContainer {
+    manifest: Manifest,
 }
 
 impl DosContainer {
-    pub fn new(manifest: &Path) -> Self {
-        DosContainer{}
+    pub fn new(manifest: &Path) -> Result<Self, std::io::Error> {
+        Ok(DosContainer {
+            manifest: Manifest::load(manifest)?,
+        })
     }
 }
---
+++ manifest/src/lib.rs
@@ -0,0 +1,254 @@
+use std::{fmt, fs::File, io::Read, path::{Path, PathBuf}};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GameMetadata {
+    title: String,
+    publisher: String,
+    year: u32,
+    comment: Option<String>,
+}
+
+impl fmt::Display for GameMetadata {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Title            : {}\nPublisher        : {}\nYear             : {}\nComment          : {}\n",
+            self.title, self.publisher, self.year, self.comment.as_deref().unwrap_or("N/A")
+        )
+    }
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Layer {
+    url: String,
+    checksum: Option<String>,
+    label: Option<String>,
+}
+
+impl fmt::Display for Layer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "URL              : {}\nLabel            : {}\nChecksum         : {}\n",
+            self.url,
+            self.label.as_deref().unwrap_or("N/A"),
+            self.checksum.as_deref().unwrap_or("N/A")
+        )
+    }
+}
+
+impl Layer {
+    pub fn url(&self) -> String {
+        self.url.to_owned()
+    }
+
+    pub fn label(&self) -> String {
+        if self.label.is_some() {
+            self.label.clone().unwrap()
+        } else {
+            self.url.to_owned()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct GameConfig {
+    autoexec: String,
+    config: String,
+}
+
+impl fmt::Display for GameConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Autoexec         : {}\nConfig           : {}\n",
+            self.autoexec, self.config
+        )
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct OsSettings {
+    full_install: Option<bool>,
+}
+
+impl fmt::Display for OsSettings {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Full Install     : {}\n",
+            self.full_install.unwrap_or(false)
+        )
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Geometry {
+    pub cylinders: usize,
+    pub heads: usize,
+    pub sectors: usize,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Disk {
+    pub name: PathBuf,
+    pub label: Option<String>,
+    pub size: Option<u32>,
+    #[serde(default = "default_filesystem")]
+    pub filesystem: String,
+    #[serde(default = "default_disktype")]
+    pub disktype: String,
+    #[serde(default= "default_harddisktype")]
+    pub harddisktype: String,
+    pub geometry: Option<Geometry>,
+}
+
+impl fmt::Display for Disk {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Disk Name        : {:?}\nLabel            : {}\nSize (MB)        : {}\nDisk Type        : {}\n",
+            self.name,
+            self.label.as_deref().unwrap_or("N/A"),
+            self.size.unwrap_or(0),
+            self.display_disktype()
+        )
+    }
+}
+
+impl Disk {
+    pub fn display_disktype(&self) -> String {
+        match self.disktype.to_ascii_lowercase().as_str() {
+            "ide" => "Hard disk: modern IDE/ATA.".to_string(),
+            "pcxt" => "Hard disk: IBM XT compatible".to_string(),
+            "pcat" => "Hard disk: IBM AT compatible".to_string(),
+            "f35_720" => "Floppy: 3.5\" DD 720KB".to_string(),
+            "f35_1440" => "Floppy: 3.5\" HD 1.44MB".to_string(),
+            "f35_2880" => "Floppy: 3.5\" XD 2.88MB".to_string(),
+            "f525_160" => "Floppy: 5.25\" 160KB".to_string(),
+            "f525_180" => "Floppy: 5.25\" 180KB".to_string(),
+            "f525_320" => "Floppy: 5.25\" 320KB".to_string(),
+            "f525_360" => "Floppy: 5.25\" 360KB".to_string(),
+            "f525_1200" => "Floppy: 5.25\" 1.2MB".to_string(),
+            _ => "Unknown disk type. Please report this as a bug!".to_string(),
+        }
+    }
+}
+        
+#[derive(Debug, Deserialize, Serialize)]
+pub struct OperatingSystem {
+    pub version: String,
+    #[serde(default = "default_osvariant")]
+    pub variant: String,
+}
+
+impl fmt::Display for OperatingSystem {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "OS Version       : {}\nVariant          : {}\n",
+            self.display_version(),
+            self.display_variant()
+        )
+    }
+}
+
+impl OperatingSystem {
+    pub fn display_version(&self) -> String {
+        match self.version.as_str() {
+            "IBMDOS100" => "IBM PC-DOS 1.00".to_string(),
+            "IBMDOS110" => "IBM PC-DOS 1.10".to_string(),
+            "IBMDOS200" => "IBM PC-DOS 2.00".to_string(),
+            _ => self.version.clone(), // Fallback to the original version if no match
+        }
+    }
+
+    pub fn display_variant(&self) -> String {
+        match self.variant.as_str() {
+            "minimal" => "Minimal installation.".to_string(),
+            "fullinstall" => "Full installation".to_string(),
+            _ => self.version.clone(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Manifest {
+    pub version: Option<u32>,
+    pub metadata: GameMetadata,
+    pub layers: Vec<Layer>,
+    pub disk: Disk,
+    pub os: OperatingSystem,
+    #[serde(rename = "AUTOEXEC")]
+    pub autoexec: Option<String>,
+    #[serde(rename = "CONFIG")]
+    pub config: Option<String>,
+}
+
+fn default_disktype() -> String {
+    "HardDisk".to_string()
+}
+
+fn default_filesystem() -> String {
+    "FAT12".to_string()
+}
+
+fn default_harddisktype() -> String {
+    "CUSTOM".to_string()
+}
+
+impl fmt::Display for Manifest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Manifest Version : {}\n", self.version.unwrap_or(0))?;
+        write!(f, "{}", self.metadata)?;
+        write!(f, "Layers           :\n")?;
+        for (i, layer) in self.layers.iter().enumerate() {
+            write!(f, "Layer {}:\n{}", i + 1, layer)?;
+        }
+        write!(f, "{}", self.disk)?;
+        write!(f, "{}", self.os)?;
+        write!(
+            f,
+            "AUTOEXEC         : {}\n",
+            self.autoexec.as_deref().unwrap_or("N/A")
+        )?;
+        write!(
+            f,
+            "CONFIG           : {}\n",
+            self.config.as_deref().unwrap_or("N/A")
+        )?;
+        Ok(())
+    }
+}
+
+impl Manifest {
+    pub fn load(yaml_path: &Path) -> Result<Manifest, std::io::Error> {
+        let mut file = File::open(yaml_path)?;
+        let mut yaml = String::new();
+        file.read_to_string(&mut yaml)?;
+        let manifest: Manifest =
+            serde_yaml::from_str(&yaml).expect("Failed to convert YAML to Manifest.");
+        Ok(manifest)
+    }
+
+    pub fn metadata(&self) -> &GameMetadata {
+        &self.metadata
+    }
+
+    pub fn default_disktype() -> String {
+        "ide".to_string()
+    }
+
+    pub fn disktype(&self) -> String {
+        self.disk.disktype.clone()
+    }
+
+    pub fn diskfile(&self) -> PathBuf {
+        self.disk.name.clone()
+    }
+}
+
+pub fn default_osvariant() -> String {
+    "minimal".to_string()
+}
