dff43d207048e5d6d5b8c13ea4ee96d63595cfdb
Bas v.d. Wiel <bas@area536.com>
2025-01-03 11:19:01 +0100

    Start wiring parts of the old CLI utility in as a testing tool for now.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,31 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use core::DosContainer;
+
+#[derive(Parser)]
+#[command(version="v0.0.1 'Smeagol'", author="Bas v.d. Wiel <bas@area536.com>", about="DOSContainer CLI utility", long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Build a DOSContainer manifest into a disk image.
+    Build {
+        /// Filename of the build manifest, this is the YAML file you wrote.
+        name: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Build { name } => {
+            let container = DosContainer::new(name);
+            println!("{:?}", container);
+        }
+    }
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,11 @@
+use std::path::Path;
+
+#[derive(Debug)]
 pub struct DosContainer {
-    
+}
+
+impl DosContainer {
+    pub fn new(manifest: &Path) -> Self {
+        DosContainer{}
+    }
 }
