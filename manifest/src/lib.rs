06f78530b6be82ef38f0d65ccc29d98ecac664de
Bas v.d. Wiel <bas@area536.com>
2025-04-05 17:00:21 +0200

    Finish field ergonomics and ensure it gets tested.

--- manifest/src/lib.rs
+++ manifest/src/lib.rs
@@ -74,6 +74,17 @@ impl PartialEq<i64> for FieldRef<'_> {
     }
 }
 
+impl PartialEq<f64> for FieldRef<'_> {
+    fn eq(&self, other: &f64) -> bool {
+        matches!(self.0, Some(FieldValue::Float(f)) if f == other)
+    }
+}
+
+impl PartialEq<bool> for FieldRef<'_> {
+    fn eq(&self, other: &bool) -> bool {
+        matches!(self.0, Some(FieldValue::Boolean(b)) if b == other)
+    }
+}
 pub struct Manifest {
     version: usize,
     layers: Vec<Layer>,
@@ -115,10 +126,37 @@ mod test {
     use super::*;
 
     #[test]
-    fn field() {
+    fn str_field() {
         let mut layer = Layer::new("foundation");
         layer.insert_field("os", FieldValue::String(String::from("test")));
-
         assert_eq!(layer.field("os"), "test");
+    }
+
+    #[test]
+    fn string_field() {
+        let mut layer = Layer::new("foundation");
+        layer.insert_field("os", FieldValue::String(String::from("test")));
+        assert_eq!(layer.field("os"), String::from("test"));
+    }
+
+    #[test]
+    fn int_field() {
+        let mut layer = Layer::new("foundation");
+        layer.insert_field("os", FieldValue::Integer(5));
+        assert_eq!(layer.field("os"), 5);
+    }
+
+    #[test]
+    fn float_field() {
+        let mut layer = Layer::new("foundation");
+        layer.insert_field("os", FieldValue::Float(3.1415952));
+        assert_eq!(layer.field("os"), 3.1415952);
+    }
+
+    #[test]
+    fn bool_field() {
+        let mut layer = Layer::new("foundation");
+        layer.insert_field("os", FieldValue::Boolean(false));
+        assert_eq!(layer.field("os"), false);
     }
 }
