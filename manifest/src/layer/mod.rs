35d37b724c63506fc9affbd2ba31434ec7adbcc0
Bas v.d. Wiel <bas@area536.com>
2025-04-13 19:58:11 +0200

    Temp stash

--- manifest/src/layer/mod.rs
+++ manifest/src/layer/mod.rs
@@ -1,5 +1,6 @@
 use crate::ManifestError;
 use attohttpc::get;
+use ftp::{FtpError, FtpStream};
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
@@ -17,7 +18,7 @@ pub enum LayerType {
 pub struct Layer {
     layer_type: LayerType,
     url: Option<Url>,
-    staging_path: Option<PathBuf>,
+    zipfile_path: Option<PathBuf>,
     disk_category: Option<String>,
     disk_type: Option<String>,
     filesystem: Option<String>,
@@ -67,13 +68,13 @@ impl Layer {
     pub fn download(&mut self) -> Result<(), ManifestError> {
         if self.layer_type == Software {
             if let Some(download_url) = &self.url {
-                let staging_path = match download_url.scheme() {
+                let zipfile_path = match download_url.scheme() {
                     "http" | "https" => self.download_http()?,
                     "ftp" => self.download_ftp()?,
                     // We have a URL, but we don't support the scheme type (yet).
                     _ => return Err(ManifestError::UnsupportedUrlScheme),
                 };
-                self.staging_path = Some(staging_path);
+                self.zipfile_path = Some(zipfile_path);
             } else {
                 // We have the correct type of layer, but no URL is present.
                 return Err(ManifestError::MissingUrl);
@@ -88,7 +89,6 @@ impl Layer {
     /// Download the Layer's url over HTTP(S)
     fn download_http(&mut self) -> Result<PathBuf, ManifestError> {
         let download_path = tempdir().map_err(|_| ManifestError::TempDirError)?;
-        let staging_path = tempdir().map_err(|_| ManifestError::TempDirError)?;
 
         if let Some(url) = &self.url {
             // Extract the file name from the URL's path.
@@ -111,10 +111,10 @@ impl Layer {
 
             let mut file = File::create(&zipfile_path).map_err(|_| ManifestError::DownloadError)?;
             // Write the response body to the file.
-            let mut content = response
+            let content = response
                 .bytes()
                 .map_err(|_| ManifestError::HttpRequestError)?;
-            file.write_all(&mut content)
+            file.write_all(&content)
                 .map_err(|_| ManifestError::DownloadError)?;
 
             return Ok(zipfile_path);
@@ -124,8 +124,78 @@ impl Layer {
 
     /// Download the Layer's url over FTP
     fn download_ftp(&mut self) -> Result<PathBuf, ManifestError> {
-        println!("[TODO] Downloading over FTP.");
-        Ok(PathBuf::new())
+        let download_path = tempdir().map_err(|_| ManifestError::TempDirError)?;
+        if let Some(url) = &self.url {
+            // Extract the file name from the URL's path
+            let path = url.path();
+            let file_name = path.split('/').last().ok_or(ManifestError::InvalidUrl)?;
+            if file_name.is_empty() {
+                return Err(ManifestError::InvalidUrl);
+            }
+
+            // Compose the full path for the ZIP file
+            let zipfile_path = download_path.path().join(file_name);
+
+            // Perform the FTP download
+            let hostname = url.host_str().ok_or(ManifestError::InvalidUrl)?;
+            let port = url.port_or_known_default().unwrap_or(21);
+            let path = url.path();
+            if path.is_empty() {
+                return Err(ManifestError::InvalidUrl);
+            }
+
+            // Extract the file name from the URL's path.
+            let file_name = path.split('/').last().ok_or(ManifestError::InvalidUrl)?;
+            if file_name.is_empty() {
+                return Err(ManifestError::InvalidUrl);
+            }
+
+            let file_path = download_path.path().join(file_name);
+            // Perform the FTP transaction.
+            let mut ftp = FtpStream::connect((hostname, port))
+                .map_err(|_| ManifestError::FtpConnectionError)?;
+
+            // Authenticate with anonymous credentials if no username/password is provided.
+            let username = if url.username().is_empty() {
+                "anonymous"
+            } else {
+                url.username()
+            };
+            let password = url.password().unwrap_or("doscontainer@area536.com");
+            ftp.login(username, password)
+                .map_err(|_| ManifestError::FtpAuthenticationError)?;
+
+            // Switch to binary mode for file transfers.
+            ftp.transfer_type(ftp::types::FileType::Binary)
+                .map_err(|_| ManifestError::FtpTransferTypeError)?;
+
+            // Start retrieving the file.
+            ftp.retr(path, |stream| {
+                let mut local_file =
+                    File::create(&file_path).map_err(|e| FtpError::ConnectionError(e))?;
+                let mut buffer = [0u8; 8192];
+                loop {
+                    let bytes_read = stream
+                        .read(&mut buffer)
+                        .map_err(|e| FtpError::ConnectionError(e))?;
+                    if bytes_read == 0 {
+                        break; // EOF
+                    }
+                    local_file
+                        .write_all(&buffer[..bytes_read])
+                        .map_err(|e| FtpError::ConnectionError(e))?;
+                }
+                Ok(())
+            })
+            .map_err(|_| ManifestError::FtpConnectionError)?;
+
+            // Close the FTP connection gracefully.
+            ftp.quit().map_err(|_| ManifestError::FtpConnectionError)?;
+
+            // Return the path to the downloaded file.
+            return Ok(zipfile_path);
+        }
+        Err(ManifestError::InvalidUrl)
     }
 }
 
@@ -134,7 +204,7 @@ impl Default for Layer {
         Layer {
             layer_type: Software,
             url: None,
-            staging_path: None,
+            zipfile_path: None,
             disk_category: None,
             disk_type: None,
             filesystem: None,
