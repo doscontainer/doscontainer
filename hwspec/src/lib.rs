4127b5eab0d8516ea77687b6e7677d1ae28a5fa8
Bas v.d. Wiel <bas@area536.com>
2025-04-22 18:21:48 +0200

    Updates

--- hwspec/src/lib.rs
+++ hwspec/src/lib.rs
@@ -1,6 +1,7 @@
 /// This enum provides a type-safe way to handle audio devices
-#[derive(Debug)]
-pub enum AudioCard {
+#[derive(Clone, Debug)]
+pub enum AudioDeviceType {
+    Bleeper,
     AdLib,
     CMS,
     SB10,
@@ -18,4 +19,30 @@ pub enum AudioCard {
     COVOX,
     GUS,
     GUSMAX,
+}
+
+/// Fully configured audio device
+pub struct AudioDevice {
+    device: Bleeper,
+    io: Vec<u16>,
+    dma: Vec<u8>,
+    irq: Vec<u8>,
+}
+
+impl Default for AudioDevice {
+    fn default() -> AudioDevice {
+        AudioDevice {
+            device: AudioDeviceType::AdLib,
+            io: Vec::new(),
+            dma: Vec::new(),
+            irq: Vec::new(),
+        }
+    }
+}
+
+impl AudioDevice {
+    pub fn devicetype(&self) -> AudioDeviceType {
+        // Very simple enum type, cloning is cheap
+        self.device.clone()
+    }
 }
