e43154d577e609d3cb580a88a04833b4bc95113c
Bas v.d. Wiel <bas@area536.com>
2025-04-26 21:15:39 +0200

    Finish CPU FromStr test suite.

--- hwspec/src/tests.rs
+++ hwspec/src/tests.rs
@@ -1,8 +1,7 @@
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
     use crate::cpu::Cpu;
+    use std::str::FromStr;
 
     #[test]
     fn valid_8088() {
@@ -67,5 +66,72 @@ mod tests {
         for element in ["I80486DX", "I80486", "80486DX", "80486", "486DX", "486"] {
             assert_eq!(reference, Cpu::from_str(element).unwrap());
         }
+    }
+
+    #[test]
+    fn valid_80486_sx() {
+        let reference = Cpu::I80486SX;
+        for element in [
+            "i80486sx", "I80486SX", "i80486Sx", "80486sx", "486sx", "486SX",
+        ] {
+            assert_eq!(reference, Cpu::from_str(element).unwrap());
+        }
+    }
+
+    #[test]
+    fn valid_486_sl() {
+        let reference = Cpu::I80486SL;
+        for element in ["i80486sl", "I80486SL", "80486sL", "486sl", "486SL"] {
+            assert_eq!(reference, Cpu::from_str(element).unwrap());
+        }
+    }
+
+    #[test]
+    fn valid_486_sx2() {
+        let reference = Cpu::I80486SX2;
+        for element in [
+            "I80486SX2",
+            "80486SX2",
+            "486SX2",
+            "i80486sx2",
+            "80486sX2",
+            "486Sx2",
+        ] {
+            assert_eq!(reference, Cpu::from_str(element).unwrap());
+        }
+    }
+
+    #[test]
+    fn valid_486_dx2() {
+        let reference = Cpu::I80486DX2;
+        for element in ["I80486DX2", "80486DX2", "486DX2", "i80486Dx2", "486dx2"] {
+            assert_eq!(reference, Cpu::from_str(element).unwrap());
+        }
+    }
+
+    #[test]
+    fn valid_486_dx4() {
+        let reference = Cpu::I80486DX4;
+        for element in ["I80486DX4", "80486DX4", "486DX4", "i80486Dx4", "486dx4"] {
+            assert_eq!(reference, Cpu::from_str(element).unwrap());
+        }
+    }
+
+    #[test]
+    fn ambiguous_486() {
+        let dx = Cpu::I80486DX;
+        let ambiguous = Cpu::from_str("486").unwrap();
+        assert_eq!(dx, ambiguous);
+    }
+
+    #[test]
+    fn invalid_cpu_name() {
+        let invalid_cpu = "MOS6502";
+        assert!(Cpu::from_str(invalid_cpu).is_err());
+    }
+
+    #[test]
+    fn empty_string() {
+        assert!(Cpu::from_str("").is_err());
     }
 }
