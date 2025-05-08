2ec032590f9b1b71554d51ebed2579f0d053b54b
Bas v.d. Wiel <bas@area536.com>
2025-05-08 19:10:27 +0200

    New AudioDevice gets sensible default configration.

--- hwspec/src/audio.rs
+++ hwspec/src/audio.rs
@@ -9,7 +9,7 @@ use crate::error::HwSpecError;
 /// This enum provides a type-safe way to handle device identification and configuration.
 ///
 /// # Examples
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Copy, Clone, Debug, Deserialize, PartialEq)]
 pub enum AudioDeviceType {
     /// Standard PC speaker (beeper)
     Bleeper,
@@ -88,25 +88,82 @@ impl FromStr for AudioDeviceType {
 pub struct AudioDevice {
     device: AudioDeviceType,
     io: Option<u16>,
-    dma: Option<u8>,
-    irq: Option<u8>,
+    dma_low: Option<u8>,
+    dma_high: Option<u8>,
+    irq_low: Option<u8>,
+    irq_high: Option<u8>,
 }
 
 impl AudioDevice {
     /// Creates a new `AudioDevice` instance for the given `AudioDeviceType`.
     ///
-    /// Hardware resource assignments (I/O port, DMA, IRQ) are initially unset.
+    /// Hardware resource assignments (I/O port, DMA, IRQ) are set to documented
+    /// factory defaults from the original manufacturer's documentation.
     ///
     /// # Arguments
     ///
     /// * `device` - The type of audio device.
     pub fn new(device: AudioDeviceType) -> Self {
-        Self {
+        let mut new_device = AudioDevice {
             device,
             io: None,
-            dma: None,
-            irq: None,
+            dma_low: None,
+            dma_high: None,
+            irq_low: None,
+            irq_high: None,
+        };
+
+        // Set the IO/DMA/IRQ values to sensible defaults for the device chosen
+        match device {
+            AudioDeviceType::Bleeper => (),
+            AudioDeviceType::AdLib => new_device.set_io(0x388),
+            AudioDeviceType::CMS => new_device.set_io(0x220),
+            AudioDeviceType::SB10 | AudioDeviceType::SB15 | AudioDeviceType::SB20 => {
+                new_device.set_io(0x220);
+                new_device.set_dma_low(1);
+                new_device.set_irq_low(7);
+            }
+            AudioDeviceType::SBPRO | AudioDeviceType::SBPRO2 => {
+                new_device.set_io(0x220);
+                new_device.set_dma_low(1);
+                new_device.set_irq_low(5);
+            }
+            AudioDeviceType::SB16 => {
+                new_device.set_io(0x220);
+                new_device.set_dma_low(1);
+                new_device.set_dma_high(5);
+                new_device.set_irq_low(5);
+                new_device.set_irq_high(11);
+            }
+            AudioDeviceType::SBAWE32 => {
+                new_device.set_io(0x220);
+                new_device.set_irq_low(5);
+                new_device.set_irq_high(11);
+                new_device.set_dma_low(1);
+                new_device.set_dma_high(5);
+            }
+            AudioDeviceType::MT32
+            | AudioDeviceType::LAPC1
+            | AudioDeviceType::MPU401
+            | AudioDeviceType::SC55
+            | AudioDeviceType::SCC1 => {
+                new_device.set_io(0x330);
+                new_device.set_irq_low(2);
+                new_device.set_irq_high(9);
+            }
+            AudioDeviceType::COVOX => {
+                new_device.set_io(0x378);
+                new_device.set_irq_low(7);
+            }
+            AudioDeviceType::GUS | AudioDeviceType::GUSMAX => {
+                new_device.set_io(0x220);
+                new_device.set_dma_high(7);
+                new_device.set_dma_low(5);
+                new_device.set_irq_low(5);
+                new_device.set_irq_high(11);
+            }
         }
+        new_device
     }
 
     /// Returns a reference to the `AudioDeviceType` of this device.
@@ -128,17 +185,34 @@ impl AudioDevice {
     /// # Arguments
     ///
     /// * `dma` - The DMA channel number (typically 0–7).
-    pub fn set_dma(&mut self, dma: u8) {
-        self.dma = Some(dma);
+    pub fn set_dma_low(&mut self, dma: u8) {
+        self.dma_low = Some(dma);
+    }
+
+    /// Set the high (16-bit) DMA channel number for this device.
+    ///
+    /// # Arguments
+    /// * `dma` - The DMA channel number (typically 0-7).
+    pub fn set_dma_high(&mut self, dma: u8) {
+        self.dma_high = Some(dma);
     }
 
     /// Sets the IRQ line number for this device.
     ///
     /// # Arguments
     ///
-    /// * `irq` - The IRQ line number (typically 0–15).
-    pub fn set_irq(&mut self, irq: u8) {
-        self.irq = Some(irq);
+    /// * `irq` - The IRQ line number (typically 0–7).
+    pub fn set_irq_low(&mut self, irq: u8) {
+        self.irq_low = Some(irq);
+    }
+
+    /// Sets the 16-bit IRQ line number for this device.
+    ///
+    /// # Arguments
+    ///
+    /// * `irq` - The IRQ line number (typically 8-15)
+    pub fn set_irq_high(&mut self, irq: u8) {
+        self.irq_high = Some(irq);
     }
 
     /// Convenience method to set all hardware resources for this device at once.
@@ -148,10 +222,12 @@ impl AudioDevice {
     /// * `io` - The I/O port address.
     /// * `dma` - The DMA channel number.
     /// * `irq` - The IRQ line number.
-    pub fn configure(&mut self, io: u16, dma: u8, irq: u8) {
+    pub fn configure(&mut self, io: u16, dma_low: u8, dma_high: u8, irq_low: u8, irq_high: u8) {
         self.set_io(io);
-        self.set_dma(dma);
-        self.set_irq(irq);
+        self.set_dma_low(dma_low);
+        self.set_dma_high(dma_high);
+        self.set_irq_low(irq_low);
+        self.set_irq_high(irq_high);
     }
 
     /// Returns the configured I/O port address, if any.
@@ -160,13 +236,23 @@ impl AudioDevice {
     }
 
     /// Returns the configured DMA channel number, if any.
-    pub fn dma(&self) -> Option<u8> {
-        self.dma
+    pub fn dma_low(&self) -> Option<u8> {
+        self.dma_low
+    }
+
+    /// Returns the configured DMA channel (16-bit), if any.
+    pub fn dma_high(&self) -> Option<u8> {
+        self.dma_high
     }
 
     /// Returns the configured IRQ line number, if any.
-    pub fn irq(&self) -> Option<u8> {
-        self.irq
+    pub fn irq_low(&self) -> Option<u8> {
+        self.irq_low
+    }
+
+    /// Returns the configured 16-bit IRQ line number, if any.
+    pub fn irq_high(&self) -> Option<u8> {
+        self.irq_high
     }
 }
 
@@ -175,11 +261,6 @@ impl FromStr for AudioDevice {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let devicetype = AudioDeviceType::from_str(s)?;
-        return Ok(Self {
-            device: devicetype,
-            io: None,
-            dma: None,
-            irq: None,
-        });
+        return Ok(AudioDevice::new(devicetype));
     }
 }
