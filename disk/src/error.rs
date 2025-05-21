a5de94a34d9dfd642410ccf4403b8ffebe16f23a
Bas v.d. Wiel <bas@area536.com>
2025-05-21 16:56:40 +0200

    Add tests to SectorSize

--- disk/src/error.rs
+++ disk/src/error.rs
@@ -1,3 +1,13 @@
+#[derive(Debug,PartialEq)]
 pub enum DiskError {
+    BufferTooSmall,
+    FileMetadataFailed,
+    FileOpenFailed,
+    FlushFailed,
+    InvalidFileSize,
     InvalidSectorSize,
+    OutOfBounds,
+    ReadFailed,
+    SeekFailed,
+    WriteFailed,
 }
--- disk/src/sectorsize.rs
+++ disk/src/sectorsize.rs
@@ -16,6 +16,13 @@ pub enum SectorSize {
     S4096,
 }
 
+/// Just about everyone uses 512-byte sectors, implement the Default trait for that.
+impl Default for SectorSize {
+    fn default() -> Self {
+        SectorSize::S512
+    }
+}
+
 impl SectorSize {
     pub fn as_usize(&self) -> usize {
         match self {
@@ -48,5 +55,53 @@ impl TryFrom<usize> for SectorSize {
             4096 => Ok(SectorSize::S4096),
             _ => Err(DiskError::InvalidSectorSize),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn default_is_512() {
+        assert_eq!(SectorSize::default(), SectorSize::S512);
+    }
+
+    #[test]
+    fn as_usize_returns_correct_values() {
+        assert_eq!(SectorSize::S128.as_usize(), 128);
+        assert_eq!(SectorSize::S256.as_usize(), 256);
+        assert_eq!(SectorSize::S512.as_usize(), 512);
+        assert_eq!(SectorSize::S1024.as_usize(), 1024);
+        assert_eq!(SectorSize::S2048.as_usize(), 2048);
+        assert_eq!(SectorSize::S4096.as_usize(), 4096);
+    }
+
+    #[test]
+    fn try_from_valid_sizes() {
+        assert_eq!(SectorSize::try_from(128).unwrap(), SectorSize::S128);
+        assert_eq!(SectorSize::try_from(256).unwrap(), SectorSize::S256);
+        assert_eq!(SectorSize::try_from(512).unwrap(), SectorSize::S512);
+        assert_eq!(SectorSize::try_from(1024).unwrap(), SectorSize::S1024);
+        assert_eq!(SectorSize::try_from(2048).unwrap(), SectorSize::S2048);
+        assert_eq!(SectorSize::try_from(4096).unwrap(), SectorSize::S4096);
+    }
+
+    #[test]
+    fn try_from_invalid_size_returns_error() {
+        let err = SectorSize::try_from(100);
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err(), DiskError::InvalidSectorSize);
+    }
+
+    #[test]
+    fn display_returns_expected_string() {
+        assert_eq!(format!("{}", SectorSize::S128), "128 bytes");
+        assert_eq!(format!("{}", SectorSize::S256), "256 bytes");
+        assert_eq!(format!("{}", SectorSize::S512), "512 bytes");
+        assert_eq!(format!("{}", SectorSize::S1024), "1024 bytes");
+        assert_eq!(format!("{}", SectorSize::S2048), "2048 bytes");
+        assert_eq!(format!("{}", SectorSize::S4096), "4096 bytes");
     }
 }
