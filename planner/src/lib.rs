8a2cb30850bd3e962981dd70d3dadce099fe6d28
Bas v.d. Wiel <bas@area536.com>
2025-05-25 14:43:49 +0200

    Testing ibmwipe

--- planner/src/lib.rs
+++ planner/src/lib.rs
@@ -15,7 +15,7 @@ impl InstallationPlanner {
     /// Pass an HwSpec and an OsSupport into this function to figure out
     /// if the requested OS will run on the provided hardware. This function
     /// filters out operating systems so that only a compatible set remains.
-    pub fn is_compatible(hwspec: &HwSpec, os: &OsSupport) -> bool {
+    fn is_compatible(hwspec: &HwSpec, os: &OsSupport) -> bool {
         hwspec.ram() >= os.min_ram_kib
             && os.supported_cpu_families.contains(&hwspec.cpu().family())
             && hwspec
@@ -35,6 +35,7 @@ impl InstallationPlanner {
 
         let mut disk = RawImage::new(Path::new("/home/bvdwiel/test.img"), SectorSize::S512, 320)
             .expect("Failed to create disk");
+        disk.ibmwipe().unwrap();
         let os = OperatingSystem::from_vendor_version("IBM", "1.00").unwrap();
         disk.write_sector(0, os.bootsector()).unwrap();
 
