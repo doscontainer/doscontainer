10b5f45197a6e329657db09ea0a0d5047a7a28eb
Bas v.d. Wiel <bas@are536.com>
2025-05-30 19:33:16 +0200

    Try thiserror for common errors.

--- common/src/error.rs
+++ common/src/error.rs
@@ -1,25 +1,22 @@
-use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum CommonError {
+    #[error("Clock rate is too high")]
     ClockTooHigh,
+
+    #[error("Clock rate is too low")]
     ClockTooLow,
+
+    #[error("Invalid audio device: {0}")]
     InvalidAudioDevice(String),
+
+    #[error("Invalid CPU specified")]
     InvalidCpu,
+
+    #[error("Invalid floppy type")]
     InvalidFloppyType,
+
+    #[error("Invalid video device.")]
     InvalidVideoDevice,
-}
-
-
-impl fmt::Display for CommonError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            CommonError::ClockTooHigh => write!(f, "Clock rate is too high"),
-            CommonError::ClockTooLow => write!(f, "Clock rate is too low"),
-            CommonError::InvalidAudioDevice(dev) => write!(f, "Invalid audio device: {}", dev),
-            CommonError::InvalidCpu => write!(f, "Invalid CPU specified"),
-            CommonError::InvalidFloppyType => write!(f, "Invalid floppy type"),
-            CommonError::InvalidVideoDevice => write!(f, "Invalid video device."),
-        }
-    }
 }
